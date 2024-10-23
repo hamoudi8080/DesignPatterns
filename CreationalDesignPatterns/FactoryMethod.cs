aa971886af0910a98c9df7958ca7e7760268c882
hamod <hamoderaso@outlook.com>
2024-10-23 15:32:28 +0200

    first commit factory pattern

---
+++ CreationalDesignPatterns/FactoryMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalDesignPatterns
+{
+    public class FactoryMethod
+    {
+        public static void Man(string[] args)
+        {
+            Console.WriteLine("Factory Method Design Pattern");
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("Creating a new ConcreteCreator1 object");
+ 
+            Console.WriteLine("Calling the FactoryMethod of ConcreteCreator1 object");
+         
+
+            Console.WriteLine("Creating a new ConcreteCreator2 object");
+      
+            Console.WriteLine("Calling the FactoryMethod of ConcreteCreator2 object");
+            
+            Console.WriteLine("-------------------------------");
+        }
+
+    }
+}
